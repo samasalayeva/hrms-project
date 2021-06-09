8f6d39fa9da266526dac3593e33643ae58a393fc
Sema Salayeva <semasalayeva95@gmail.com>
2021-06-09 20:33:57 +0400

    update java project homework 8

--- src/main/java/kodlamaio/hrms/business/concretes/CityManager.java
+++ src/main/java/kodlamaio/hrms/business/concretes/CityManager.java
@@ -30,8 +30,14 @@ public class CityManager implements CityService {
     @Override
     public Result add(CityDto cityDto) {
         City city = new City();
-        city.setCityName(cityDto.getCityName());
+        city.setCityName(capitalize(cityDto.getCityName()));
         cityDao.save(city);
         return new SuccessResult("sehir eklendi");
+    }
+
+    public static String capitalize(String str)
+    {
+        if(str == null) return str;
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 }
